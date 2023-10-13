864c4a4836b92bd01b36641feeff651cafc1ef8e
unknown <frez1374@gmail.com>
2023-10-13 07:19:28 +0330

    Add TwoNumberSummation class

---
+++ TwoNumberSummation.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class TwoNumberSummation
+{
+    public function solve(int $x, array $list): bool
+    {
+        $existenceCheck = [];
+
+        for ($i = 0; $i < count($list); $i++) {
+            $complement = $x - $list[$i];
+            if (isset($existenceCheck[$complement])) {
+                return true;
+            }
+            $existenceCheck[$list[$i]] = true;
+        }
+
+        return false;
+    }
+}
